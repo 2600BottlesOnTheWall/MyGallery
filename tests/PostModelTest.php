c2e718d5c01cc672bd3abadb29f7fafe5137e678
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-12 14:57:58 +0300

    Add delete and create sgortcodes

--- tests/PostModelTest.php
+++ tests/PostModelTest.php
@@ -17,6 +17,7 @@ class PostModelTest extends \WP_UnitTestCase
         $this->shortcode='[my-gallery ids=434,232 title="Test" config=11111]';
         $this->postId=$this->factory()->post->create(['post_title'=>'Test post','post_content'=>$this->shortcode]);
         $this->instance=new PostModel($this->postId);
+        $this->newShortcode='[my-gallery ids=434,232,435 title="New test" config=11611]';
 
     }
     public function  testGetShortcodes(){
@@ -25,15 +26,41 @@ class PostModelTest extends \WP_UnitTestCase
         $this->assertInstanceOf(stdClass::class,$shortcode[0]);
     }
     public function testUpdatePostShortcodes(){
-        $new_shortcode='[my-gallery ids=434,232,435 title="New test" config=11611]';
+        $postId=$this->factory()->post->create(['post_title'=>'Test post','post_content'=>$this->shortcode]);
+        $postInstance=new PostModel($postId);
+      
         $shortcode=(object) array(
-            'code'=>$new_shortcode,
+            'code'=>$this->newShortcode,
             'status'=>'changed'
         );
-     
+        $postInstance->updatePostShortcodes(array($shortcode));
+        $post=get_post($postId);
+    
+        $this->assertEquals($this->newShortcode,$post->post_content);
+    }
+    public function testAddNewPostShortcodes(){
+        $postId=$this->factory()->post->create(['post_title'=>'Test post','post_content'=>$this->shortcode]);
+        $postInstance=new PostModel($postId);
+       
+        $shortcode=(object) array(
+            'code'=>$this->newShortcode,
+            'status'=>'draft'
+        );
         $this->instance->updatePostShortcodes(array($shortcode));
         $post=get_post($this->postId);
-        $this->assertEquals($new_shortcode,$post->post_content);
+        $this->assertEquals($this->shortcode.'<p>'.$this->newShortcode.'</p>',$post->post_content);
+    }
+    public function testDeletePostShortcodes(){
+        $postId=$this->factory()->post->create(['post_title'=>'Test post','post_content'=>$this->shortcode]);
+        $postInstance=new PostModel($postId);
+       
+        $shortcode=(object) array(
+            'code'=>$this->shortcode,
+            'status'=>'deleted'
+        );
+        $this->instance->updatePostShortcodes(array($shortcode));
+        $post=get_post($this->postId);
+        $this->assertEmpty($post->post_content);
     }
     public function tearDown()
     {
