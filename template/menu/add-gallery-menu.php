344330a46fb75b329baa20d0f5a5c098b3afde19
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-16 13:22:31 +0300

    fix path to banner

--- template/menu/add-gallery-menu.php
+++ template/menu/add-gallery-menu.php
@@ -1,7 +1,7 @@
 
 <div>
 <div class="my-gallery-header-banner">
-    <img src="<?php echo plugins_url('../../public/images/banner_top.png', __FILE__ ) ?>" alt='My gallery header banner'>
+    <img src="<?php echo MYGALLERY_PLUGIN_URL.'/public/images/banner_top.png' ?>" alt='My gallery header banner'>
 </div>
 <div class='wrap wrap-add-my-gallery' id="add-gallery" data-nonce="<?php echo wp_create_nonce('wp_rest') ?>" data-lang="<?php echo get_locale(); ?>">
 </div>
