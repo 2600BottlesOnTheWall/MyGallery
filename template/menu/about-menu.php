c6b8f6de4d4ebfd311bb19dad7e5b97160aaca9e
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-21 18:07:28 +0300

    change banners

--- template/menu/about-menu.php
+++ template/menu/about-menu.php
@@ -5,7 +5,7 @@
     
     <div  itemprop="text">
     <p>
-        <img src="<?php echo MYGALLERY_PLUGIN_URL.'/public/images/banner_top.png' ?>" alt="'Alt text'" style="max-width:100%;">
+        <img src="<?php echo MYGALLERY_PLUGIN_URL.'/public/images/banner_header_2.0.png' ?>" alt="'Alt text'" style="max-width:100%;">
     </p>
     <p>
         My Slider Gallery is a wordpress plugin that allows you to create a slider gallery anywhere in your post.
