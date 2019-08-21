23ed2546b632cb5e9886f6d5365174b71f97448d
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-21 18:25:56 +0300

    fix typos and links

--- assets/src/packages/add-gallery/src/components/cards/RateCard.js
+++ assets/src/packages/add-gallery/src/components/cards/RateCard.js
@@ -3,9 +3,9 @@ import {Card} from '../elements/Card';
 
 export default function RateCard(){
     return (
-        <Card title="Rate My-Gallery">     
+        <Card title="Rate My Slider Gallery">     
             <h6 className="mt-3">
-                Please rate My-Gallery on <a href="https://wordpress.org/plugins/mygallery/#reviews">Wordpress.org</a>
+                Please rate plugin on  <a href="https://wordpress.org/plugins/mygallery/#reviews">Wordpress.org</a>
             </h6>
             <p className="card-text mb-0">Support me and appreciate my efforts to create a quality product. Thanks for your feedback.</p>
             <div className="d-flex justify-content-left">  
