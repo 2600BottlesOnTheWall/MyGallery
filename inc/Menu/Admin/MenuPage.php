aac6f80edc813d8f7c605cc227822e14e4d76b6f
Evgeniy Zalevskiy <2600@ukr.net>
2019-09-02 17:23:15 +0300

    add data type check

--- inc/Menu/Admin/MenuPage.php
+++ inc/Menu/Admin/MenuPage.php
@@ -43,6 +43,8 @@ class MenuPage implements MenuPageInterface
      */
     public function init(MenuConfig $config)
     {
+        $config=$config->get();
+        if(!is_object($config)) throw new Exception ('Wrong config data format.Should be instance of stdClass');
         $this->config = $config->get();
         \add_action('admin_menu', array($this, 'addMainMenu'));
     }
